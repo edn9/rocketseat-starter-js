9c75a29bbc66012dbdfe174a8993cdb19c1c8068
edn9 <eduardohenriquen9@outlook.com>
2019-08-01 23:11:22 -0300

    update & last stuff mod1

--- Curso JavaScript ES6/mod1/bundle.js
+++ Curso JavaScript ES6/mod1/bundle.js
@@ -1,13 +1,3 @@
 "use strict";
 
-//7º exercício
-var nome = 'Diego';
-var idade = 23;
-var usuario = {
-  //nome: nome,
-  nome: nome,
-  //idade: idade,
-  idade: idade,
-  cidade: 'Rio do Sul'
-};
-console.log(usuario);
+console.log('Hi.');
---
+++ Curso JavaScript ES6/mod1/main-atv7.js
@@ -0,0 +1,12 @@
+//7º exercício
+const nome = 'Diego';
+const idade = 23;
+const usuario = {  
+    //nome: nome,
+    nome,  
+    //idade: idade,
+    idade,  
+    cidade: 'Rio do Sul',
+};
+
+console.log(usuario);
